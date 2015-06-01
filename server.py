4b139578da1a138ad3e6fa85c349b2d41d615a23
Tushar Karkera <tusharkarkera1610@gmail.com>
2015-05-31 21:17:33 -0700

    second commit for milestone 2, 5

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from socket import *
-
+import os
 serverSocket = socket(AF_INET, SOCK_STREAM)     #create a socket
 
 #Prepare server socket
@@ -8,8 +8,20 @@ serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 
 while True:
-    connection, address = serversocket.accept()
-    buf = connection.recv(64)
-    if len(buf) > 0:
-        print buf
-        break
+    print 'Ready to serve . . .'
+    connectionSocket, addr = serverSocket.accept()  #create socket for client
+    try:
+        message =connectionSocket.recv(1024)   #receive message from client
+        print message
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata =f.read()
+        #Send contents of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i])
+        connectionSocket.close()
+        print 'File Received'
+    except IOError:
+        connectionSocket.send('\n404 File Not Found\n')
+        connectionSocket.close()
+serverSocket.close()
